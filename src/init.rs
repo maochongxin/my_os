fd55b3c1f82328d203399e06bbe4cfb474c2fea1
maochongxin <maochongxin@gmail.com>
2019-05-23 10:02:44 +0000

    tiny kernel for riscv32

---
+++ src/init.rs
@@ -0,0 +1,21 @@
+use crate::interrupt::init as interrupt_init;
+use crate::clock::init as clock_init;
+use crate::memory::init as memory_init;
+
+global_asm!(include_str!("boot/entry.asm"));
+
+#[no_mangle]
+pub extern "C" fn rust_main(hartid: usize, dtb: usize) -> ! {
+    interrupt_init();
+    println!("Hello RISCV ! in hartid {}, dtb @ {:#x} ", hartid, dtb);
+    memory_init(dtb);
+    clock_init();
+    
+    
+    let x_ptr: *mut u32 = 0xc0020020 as *mut u32;
+
+    unsafe { asm!("ebreak"::::"volatile"); }
+    loop {}
+}
+
+
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+#![no_std]
+#![no_main]
+
+#[allow(unused_imports)]
+use mcx_os;
+
+
+
